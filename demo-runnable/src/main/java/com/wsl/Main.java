13e8f1b892fa157b9a56200eca4e3bac6bc59c4f
wsl <today_wsl@163.com>
2023-08-29 23:18:15 +0800

    maven通过pom.xml指定main class

--- demo-runnable/src/main/java/com/wsl/Main.java
+++ demo-runnable/src/main/java/com/wsl/Main.java
@@ -5,3 +5,9 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+// 运行程序
+// pom.xml增加classMain配置
+// mvn build
+// cd target
+// java -jar demo-runnable-1.0-SNAPSHOT.jar
